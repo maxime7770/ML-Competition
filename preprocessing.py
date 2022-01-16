07831f0a48470ef2dd2b267f45a78fe1657d9e21
AymericPalaric <94397830+AymericPalaric@users.noreply.github.com>
2022-01-16 14:40:47 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -522,5 +522,30 @@ train_df["change_type"] = train_df["change_type"].apply(
     lambda x: change_type_map[x])
 
 
+############  FEATURETOOLS  ###############
+import featuretools as ft
+import woodwork
+
+# J'ai gardé les dates sous forme purement numérique, parce qu'il faut un time_index
+ES = ft.EntitySet(id='train df')
+
+logical_types = {}
+
+ES = ES.add_dataframe(dataframe_name='train date1', dataframe=train_df[['index', 'date1', 'change_status_date1', 'area', 'length']], index='index', logical_types={
+                      'change_status_date1': woodwork.logical_types.Categorical, 'date1': woodwork.logical_types.Datetime}, time_index='date1')
+ES = ES.add_dataframe(dataframe_name='train date2', dataframe=train_df[['index', 'date2', 'change_status_date2', 'area', 'length']], make_index=True, index='index2', logical_types={
+                      'change_status_date2': woodwork.logical_types.Categorical, 'date2': woodwork.logical_types.Datetime}, time_index='date2')
+
+ES = ES.add_relationship('train date1', 'index', 'train date2', 'index')
+print(ES)
+
+feature_matrix, feature_defs = ft.dfs(entityset=ES,
+                                      target_dataframe_name='train date1',
+                                      verbose=1
+                                      )
+print(feature_matrix)
+######################################
+
+
 train_df.to_csv("train_df.csv")
 test_df.to_csv("test_df.csv")
